34261bafd2fed80444f9714c74cefdf8bdf6518c
kdeslauriers <kdeslauriers@bayviewglen.ca>
2014-10-20 19:22:30 -0400

    Commit First Solution - The answer was correct; however, the data file did contain incorrect formatting.  As a result, I did fix the data file and also handled the issue by calling trim() on the String before equals.

--- Senior Computer Science Club/src/com/bayviewglen/extras/ReadingFromAFile.java
+++ Senior Computer Science Club/src/com/bayviewglen/extras/ReadingFromAFile.java
@@ -1,10 +1,42 @@
 package com.bayviewglen.extras;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadingFromAFile {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("data/DATA10.txt"));
 
+		int numLate = 0;
+		int leftToServe = 0;
+		
+		int currentTicket = Integer.parseInt(scanner.nextLine());		// I have the # 23
+		boolean done = false;
+		
+		while(!done){
+			String s = scanner.nextLine();	// s is one of the options
+			if (s.trim().equals("EOF")){
+				done = true;
+			}else if (s.trim().equals("TAKE")){
+				numLate++;	// adds one
+				currentTicket++;
+				leftToServe++;
+				if (currentTicket == 1000)
+					currentTicket = 1;
+			}else if (s.trim().equals("SERVE")){
+				leftToServe--;
+			}else{
+				System.out.println(numLate + " " + leftToServe + " " + currentTicket);
+				numLate = 0;
+				leftToServe = 0;
+			}
+		}
+		
+		
+		
+		
 	}
 
 }
